12b846c01d2bedc7a02d42a7e97521b55ecda1a8
Ethan Barnes <ecbarnes@bradford.ac.uk>
2021-12-08 14:08:21 +0000

    Changed from recyclerView to ExpandableListView

---
+++ app/src/main/java/com/example/learningtolive/ExpandableListDataPump.java
@@ -0,0 +1,30 @@
+package com.example.learningtolive;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListDataPump {
+    public static HashMap<String, List<String>> getData() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+
+        List<String> housing = new ArrayList<String>();
+        housing.add("Finding a home");
+        housing.add("Ageuk");
+        housing.add("New furniture");
+
+        List<String> food = new ArrayList<String>();
+        food.add("BM stores");
+        food.add("Wilko");
+        food.add("Save the student");
+
+        List<String> eatingOut = new ArrayList<String>();
+        eatingOut.add("Where to eat in Bradford");
+        eatingOut.add("Halal food");
+
+        expandableListDetail.put("Housing and Accommodation", housing);
+        expandableListDetail.put("Food and Home", food);
+        expandableListDetail.put("Eating Out", eatingOut);
+        return expandableListDetail;
+    }
+}
