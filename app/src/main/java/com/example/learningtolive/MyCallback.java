03b7b51feb177a7b3143b90195a1049e5ebd8634
Ethan Barnes <barnes.c.ethan@gmail.com>
2022-01-31 11:49:59 +0000

    About half of code converted to kotlin

--- app/src/main/java/com/example/learningtolive/MyCallback.java
+++ app/src/main/java/com/example/learningtolive/MyCallback.java
@@ -3,5 +3,5 @@ package com.example.learningtolive;
 import java.util.HashMap;
 
 public interface MyCallback {
-    void onCallBack(String title, HashMap value);
+    void onCallBack(String title, HashMap<String, String> value);
 }
